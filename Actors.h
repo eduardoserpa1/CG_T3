c0ebd02dbc264b17956a05d4760865de6191f048
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2022-06-29 20:02:37 -0300

    limpada de actors

--- Actors.h
+++ Actors.h
@@ -7,26 +7,23 @@
 #include "Ponto.h"
 
 class Player : public Instancia {
-    public:
-	int delay = 0;
+public:
 	const float speed = 1;
 	int combustivel = 100;
 	Ponto max;
 	Ponto min;
 	Player(Modelo *mod);
-
-	bool foraDaAreaDeDesenho(Ponto max, Ponto min, Ponto p);
 	void anda(vector<Ponto> espaco, float escala);
-	bool limites_do_espaco(Ponto movimentacao, vector<Ponto> espaco, float escala);
+
+private:
+	bool foraDaAreaDeDesenho(Ponto max, Ponto min, Ponto p);
+	bool limites_do_espaco(Ponto movimentacao, vector<Ponto> espaco,
+						   float escala);
 };
 
 class Inimigo : public Instancia {
-    public:
+public:
 	Inimigo(Ponto pos, float angulo, Modelo *mod);
-
-    private:
-	int delay = 0;
-	float deltaT = 1.0;
 };
 
 #endif //_ACTORS_H_
